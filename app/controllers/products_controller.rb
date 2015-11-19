e21bd10bfa037c3a5dfa3b070ad5d53f3c25ac01
stiwwelll <marco.steffensky@gmail.com>
2015-11-19 11:31:53 +0100

    product controller changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,20 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @products }
+    end
   end
 
   # GET /products/new
