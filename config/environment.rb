486ca4043c1a6f4d387c6e30525e174c16790fd7
stiwwelll <marco.steffensky@gmail.com>
2015-11-29 23:48:57 +0100

    Dynamic contact form with automated confirmation email has been added

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Actionmailer::Base.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV ['marco.steffensky@gmail.com'],
-  :password => ENV ['forVert85294'],
+  :user_name => ENV['marco.steffensky@gmail.com'],
+  :password => ENV['forVert85294'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
