16f11e89dd6778e634cf26095838d97c84d25b3c
stiwwelll <marco.steffensky@gmail.com>
2015-11-30 15:56:30 +0100

    Dynamic contact form with automated confirmation email has been added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
+  layout 'mailer'
+
   def contact_form(email, name, message)
     @message = message
     mail(:from => email,
