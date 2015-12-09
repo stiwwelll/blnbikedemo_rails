ffcf1efb505d5964771d7cb9edcddc24b49c2545
stiwwelll <marco.steffensky@gmail.com>
2015-12-09 23:14:11 +0100

    Adding Admin capabilities to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,25 @@
 class CommentsController < ApplicationController
 
-def create
-  @product = Product.find(params[:product_id])
-  @comment = @product.comments.new(comment_params)
-  @comment.user = current_user
-  respond_to do |format|
-    if @comment.save
-      format.html { redirect_to @product, notice: 'Review was created successfully.' }
-      format.json { render :show, status: :created, location: @product }
-    else
-      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-      format.json { render json: @comment.errors, status: :unprcessable_entity }
+  def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprcessable_entity }
+      end
     end
   end
-end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
   end
 
   private
