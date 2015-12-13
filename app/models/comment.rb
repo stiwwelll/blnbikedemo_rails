499258ff747db050f185aa2e5b80daa764dfc9c5
stiwwelll <marco.steffensky@gmail.com>
2015-12-14 00:32:35 +0100

    bugfixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,14 @@ class Comment < ActiveRecord::Base
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
 
+  validate :clean_language
+
+  private
+
+  def clean_language
+    if body.include? "fuck"
+      errors.add(:clean_language, "Behave!")
+    end
+  end
+
 end
