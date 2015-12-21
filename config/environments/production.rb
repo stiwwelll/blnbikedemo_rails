f340caa7a28e5082c4a5c525726b3a8c7a66396d
stiwwelll <marco.steffensky@gmail.com>
2015-12-21 23:47:59 +0100

    Added GMail mailing in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,4 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :port => 587,
-  :authentication => :plain,
-  :user_name => ENV['GMAIL_USERNAME'],
-  :password => ENV['GMAIL_PASSWORD'],
-  :domain => 'gmail.com',
-  :enable_starttls_auto => true
-}
 end
